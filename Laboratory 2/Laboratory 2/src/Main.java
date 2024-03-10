e3c853d05cec34620d55ae48e41350c41e7e94c7
Sandu Andrei <Sandu.Co.Andrei@student.utcluj.ro>
2024-03-10 13:22:45 +0200

    Lab HW

---
+++ Laboratory 2/Laboratory 2/src/Main.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
+
+// Controller
+public class Main {
+    private static final int noOfThreads = 6;
+    private static final int processorLoad = 1000000;
+
+    public static void main(String args[]){
+        Window win = new Window(noOfThreads);
+        for(int i = 0; i < noOfThreads; i++){
+            Fir fir = new Fir(i, i+2, processorLoad);
+            fir.addObserver(win);
+            new Thread(fir).start();
+        }
+    }
+}
